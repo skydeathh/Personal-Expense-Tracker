9864c1c2fbeee9e970a78718a4de935612efd73a
skydeathh <a1b2f2f2@gmail.com>
2024-09-07 16:07:11 +0700

    added interfaces for usersrepo and transactions repo

---
+++ backend/PersonalExpenseTracker/PersonalExpenseTracker.Core/Repositories/ITransactionsRepository.cs
@@ -0,0 +1,12 @@
+﻿using PersonalExpenseTracker.Core.Entities;
+
+namespace PersonalExpenseTracker.Core.Repositories
+{
+    public interface ITransactionsRepository
+    {
+        public Task AddTransaction(Transaction transaction);
+        public Task UpdateTransaction(Transaction transaction);
+        public Task DeleteTransaction(Transaction transaction);
+        public Task<Transaction> GetTransactionsByCategory(string category);
+    }
+}
---
+++ backend/PersonalExpenseTracker/PersonalExpenseTracker.Core/Repositories/IUsersRepository.cs
@@ -0,0 +1,13 @@
+﻿using PersonalExpenseTracker.Core.Entities;
+
+namespace PersonalExpenseTracker.Core.Repositories
+{
+    public interface IUsersRepository
+    {
+        public Task AddUser(User user);
+        public Task UpdateUser(User user);
+        public Task DeleteUser(User user);
+        public Task<User> GetUserByEmail(string email);
+        public Task LoginAsync(string emailOrUsername, string passwordHash);
+    }
+}
